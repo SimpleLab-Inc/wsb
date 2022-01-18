1b5c9ae54447dbec90878b3088d375cab29fdfae
RichPauloo <richpauloo@gmail.com>
2022-01-18 15:40:49 -0800

    tbd eda and model

---
+++ src/analysis/02_eda.R
@@ -0,0 +1,20 @@
+# EDA
+
+library(tidyverse)
+library(here)
+library(fs)
+library(sf)
+
+
+# plot connection count against polygon area and convex hull area
+
+# output plots of all polygons and convex hulls to visually inspect
+
+# calculate percent increase in area from polygon to convex hull to
+# estimate distribution of uncertainty in area imposed by this approach
+
+# explore relationships between connection count and water system type
+# (i.e., small, med, large)
+
+# explore other predictors with presumed large explanatory power
+
