6a6fb898578133447fbb70034d85941147eb9adc
RichPauloo <richpauloo@gmail.com>
2022-01-31 14:11:25 -0800

    rm unnecessary dependency and improve message

--- src/01_downloaders/download_frs.R
+++ src/01_downloaders/download_frs.R
@@ -1,6 +1,5 @@
 # download FRS centroids --------------------------------------------------
 
-library(tidyverse)
 library(here)
 
 # Allow for longer timeout to map download file
@@ -10,9 +9,8 @@ options(timeout = 10000)
 frs_url <- paste0("https://edg.epa.gov/data/public/OEI/FRS/",
                  "FRS_Interests_Download.zip")
 
-
 # create dir to store file, download, and un-zip
 fs::dir_create(here("data/frs"))
 download.file(frs_url, here("data/frs/frs.zip"))
 unzip(here("data/frs/frs.zip"), exdir = here("data/frs"))
-cat("Downloaded FRS data.\n")
+cat("Downloaded and unzipped FRS data.\n")
