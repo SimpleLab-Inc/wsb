9a031ea14ce341e757c712893164f2e17339c86c
RichPauloo <richpauloo@gmail.com>
2022-01-31 20:04:03 -0800

    min subset

--- src/03_analysis/archive/frs_explore.R
+++ src/03_analysis/archive/frs_explore.R
@@ -1,12 +1,12 @@
 library(tidyverse)
-library(here)
 library(data.table)
 
 cols_keep <- c("FAC_NAME", "SDWA_IDS", "SDWA_SYSTEM_TYPES", 
                "FAC_STATE", "FAC_LAT", "FAC_LONG", "FAC_COLLECTION_METHOD",
                "FAC_ACCURACY_METERS", "FAC_PERCENT_MINORITY", "FAC_POP_DEN")
+
 echo <- here::here("data/ECHO/ECHO_EXPORTER.csv") %>% 
-  data.table::fread(select = cols_keep) %>% 
+  data.table::fread(select = cols_keep, nrows = 1e5) %>% 
   rename(PWSID = SDWA_IDS) %>% 
   filter(!is.na(PWSID))
 
