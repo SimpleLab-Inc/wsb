0c5d7a8d73a62df88851edacae1f3067816da9fc
RichPauloo <richpauloo@gmail.com>
2022-02-09 13:14:59 -0800

    correct assignment operator in function scope

--- src/functions/f_clean_whitespace_nas.R
+++ src/functions/f_clean_whitespace_nas.R
@@ -3,12 +3,12 @@ f_clean_whitespace_nas <- function(df){
   
   # if df is spatial, detach geometry before cleaning cols
   if(sum(class(mhp_sp) == "sf") == 1) {
-    geom <- df$geometry
-    df <- st_drop_geometry(df)
+    geom = df$geometry
+    df   = st_drop_geometry(df)
   }
     
   # apply whitespace and NA cleaning
-  df <- dplyr::mutate_all(df, stringr::str_trim, "both") |>
+  df = dplyr::mutate_all(df, stringr::str_trim, "both") |>
     # all whitespace becomes "", so the next pattern handles all cases
     dplyr::mutate_all(dplyr::na_if, "") |>
     dplyr::mutate_all(dplyr::na_if, "NULL") |>
@@ -17,7 +17,7 @@ f_clean_whitespace_nas <- function(df){
   
   # reattach geometry if the object is spatial
   if(exists("geom")) {
-    df <- st_as_sf(bind_cols(df, geometry = geom))
+    df = st_as_sf(bind_cols(df, geometry = geom))
   }
   
   return(df)
