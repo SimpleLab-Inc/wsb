036d93a1ceafbb6b71fb0f33e22a07150eef9e73
RichPauloo <richpauloo@gmail.com>
2022-02-09 13:13:06 -0800

    rm OHE, use rename instead of selec, apply whitespace and na cleaning

--- src/transformers/transform_mhp.R
+++ src/transformers/transform_mhp.R
@@ -1,76 +1,45 @@
-# Transform mobile home park point data  ---------------------------------------
+# Transform mobile home park point data  ----------------------------------
 
 library(fs)
 library(sf)
 library(tidyverse)
-library(mltools)
 
+# helper function
+source(here::here("src/functions/f_clean_whitespace_nas.R"))
 
 # path to save raw data and standard projection
 data_path    <- Sys.getenv("WSB_DATA_PATH")
 staging_path <- Sys.getenv("WSB_STAGING_PATH")
 epsg         <- Sys.getenv("WSB_EPSG")
 
+# Read un-zipped geodatabase, clean names, transform to standard epsg
+mhp_sp <- st_read(dsn = path(data_path, "boundary/mhp/mhp.geojson")) %>% 
+  janitor::clean_names() %>% 
+  st_transform(crs = epsg)
 
-# Read un-zipped geodatabase (~6GB so querying on water to reduce file size)
-# First look at available layers
-mhp_sp <- st_read(dsn = path(data_path, "boundary/mhp/mhp.geojson"))
-
-# Transform to standard epsg
-mhp_sp <- mhp_sp %>% st_transform(crs = st_crs(epsg))
-cat("Read labeled FRS layer and transformed to CRS:", epsg, ":\n ")
+cat("Read MHP layer, cleaned names, & transformed to CRS:", epsg, "\n ")
 
 # Visualize points
 plot(st_geometry(mhp_sp), pch = 1, col = 'blue')
 
-# Clean up attribute data ------------------------------------------------------
-# Set column names to lower
-names(mhp_sp) <- tolower(names(mhp_sp))
 
-# One hot encode categorical variables ----------------------------------------- 
-# Size and units should be correlated, but a frequent units value is -999
-# So categorical grouping may be more efficient
-# One hot encode categorical and use NA for unknowns (instead of -999)
+# Clean attribute data ----------------------------------------------------
 
 mhp_sp <- mhp_sp %>% 
-  mutate(size = as.factor(tolower(size)),
+  # clean size column and replace -999 missing units with NA
+  mutate(size  = as.factor(tolower(size)),
          units = na_if(units, -999)) %>%
-  as.data.table() %>%
-  # one hot encode variables set as factors
-  one_hot() %>%
-  as.data.frame() %>%
-  # clean up column names
-  select(
-    object_id  = objectid,
-    mhp_id     = mhpid,
-    mhp_name   = name,
-    address,
-    city,
-    state,
-    zipcode    = zip,
-    zip4,
-    telephone,
-    type,
-    status,
-    county,
-    county_fips = countyfips,
-    country,
-    latitude,
-    longitude,
-    naics_code,
-    naics_desc,
-    source,
-    source_date = sourcedate,
-    val_method,
-    val_date,
-    website,
-    units,
-    size_large   = `size_large (>100)`,
-    size_medium  = `size_medium (51-100)`,
-    size_small   = `size_small (<50)`,
-    rev_geo_flag = revgeoflag,
-    geometry
-  )
+  # clean column names
+  rename(
+    object_id    = objectid,
+    mhp_id       = mhpid,
+    mhp_name     = name,
+    zipcode      = zip,
+    county_fips  = countyfips,
+    source_date  = sourcedate,
+    rev_geo_flag = revgeoflag
+  ) %>% 
+  f_clean_whitespace_nas()
 
 # Write clean mobile home park centroids
 path_out <- path(staging_path, "mhp_clean.geojson")
