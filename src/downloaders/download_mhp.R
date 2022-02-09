d860a5041531824fe3112e3d6e5b07947a93db2f
RichPauloo <richpauloo@gmail.com>
2022-02-09 12:22:55 -0800

    minor linting

--- src/downloaders/download_mhp.R
+++ src/downloaders/download_mhp.R
@@ -1,4 +1,4 @@
-# Download mobile home parks point data ----------------------------------------
+# Download mobile home parks point data -----------------------------------
 
 library(fs)
 
@@ -8,13 +8,11 @@ data_path <- Sys.getenv("WSB_DATA_PATH")
 # Allow for longer timeout to map download file
 options(timeout = 10000)
 
-# Data Source: Oklahoma ArcGIS geojson water system boundary
+# Data Source: MPH ArcGIS geojson water system boundary
 mhp_url <- paste0("https://opendata.arcgis.com/datasets/",
-                 "4cdbccc5c538452aa91ceee277c460f9_0.geojson")
+                  "4cdbccc5c538452aa91ceee277c460f9_0.geojson")
 
 # create dir to store file and download
 dir_create(path(data_path, "boundary/mhp"))
 download.file(mhp_url, path(data_path, "/boundary/mhp/mhp.geojson"))
 cat("Downloaded mobile home park point data.\n")
-
-
