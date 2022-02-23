75569a4bedb0147d31dc0377ec509d9277179edb
Jess Goddard <jess@gosimplelab.com>
2022-02-23 10:10:49 -0500

    incorporating more frs data

--- src/downloaders/download_frs.R
+++ src/downloaders/download_frs.R
@@ -8,12 +8,23 @@ data_path <- Sys.getenv("WSB_DATA_PATH")
 # Allow for longer timeout to map download file
 options(timeout = 10000)
 
-# Data Source: EPA's Facility Reigstry Service (FRS)
+# Data Source: EPA's Facility Registry Service (FRS)
 frs_url <- paste0("https://edg.epa.gov/data/public/OEI/FRS/",
                   "FRS_Interests_Download.zip")
+frs_comb_url <- paste0("https://ofmext.epa.gov/FLA/www3/state_files/",
+                      "national_combined.zip")
 
 # create dir to store file, download, and un-zip
 dir_create(path(data_path, "frs"))
-download.file(frs_url, path(data_path, "frs/frs.zip"))
-unzip(path(data_path, "frs/frs.zip"), exdir = path(data_path, "frs"))
+
+# local path to download files
+file_frs <- path(data_path, "frs/frs.zip")
+file_frs_comb <- path(data_path, "frs/frs_comb.zip")
+
+download.file(frs_url, file_frs)
+download.file(frs_comb_url, file_frs_comb)
+
+unzip(file_frs, exdir = path(data_path, "frs"))
+unzip(file_frs_comb, exdir = path(data_path, "frs"))
+
 cat("Downloaded and unzipped FRS data.\n")
