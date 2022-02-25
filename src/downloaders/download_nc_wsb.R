759f55c956ced0dabe08e6c29fd3d28b395b36d8
RichPauloo <richpauloo@gmail.com>
2022-02-25 11:31:13 -0800

    make download message clear we're downloading polygon boundaries

--- src/downloaders/download_nc_wsb.R
+++ src/downloaders/download_nc_wsb.R
@@ -11,9 +11,9 @@ options(timeout = 10000)
 # Data Source: North Carolina ArcGIS geojson water system boundary
 # https://hub.arcgis.com/datasets/nconemap::type-a-current-public-water-systems-2004/
 nc_url <- paste0("https://opendata.arcgis.com/datasets/",
-                   "58548b90bdfd4148829103ac7f4db9ce_4.geojson")
+                 "58548b90bdfd4148829103ac7f4db9ce_4.geojson")
 
 # create dir to store file and download
 dir_create(path(data_path, "boundary/nc"))
 download.file(nc_url, path(data_path, "/boundary/nc/nc.geojson"))
-cat("Downloaded NC data.\n")
+cat("Downloaded NC polygon boundary data.\n")
