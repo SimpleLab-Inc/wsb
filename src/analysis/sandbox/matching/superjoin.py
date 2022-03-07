21f559b1371072de3359a02e2fbd9c5a421ff4b2
Ryan Shepherd <ryan@databrook.io>
2022-03-06 22:12:53 -0800

    Separate EDO portions to simplify the superjoin

--- src/analysis/sandbox/matching/superjoin.py
+++ src/analysis/sandbox/matching/superjoin.py
@@ -17,15 +17,6 @@ EPSG = os.environ["WSB_EPSG"]
 crosswalk = (pd.read_csv("../crosswalks/state_fips_to_abbr.csv")
     .set_index("code"))
 
-"""
-Data Sources:
-1. SDWIS - Official list of PWS's to classify. Narrow to Active CWS's.
-2. FRS - Has centroids for ~15k facilities.
-3. ECHO? - Similar to FRS. Better matching. The points might be a superset of FRS.
-4. TIGER - Generic city boundaries. Attempt to match on city name.
-5. Mobile Home Parks - 
-6. Various exact geometries from states
-"""
 
 #%% ##########################################
 # 1) SDWIS
@@ -39,21 +30,6 @@ Table relationships:
 - water_system : water_system_facility is 1 : 0/N (~300 pwsid's missing, N=9.8 mean (wow!))
 - water_system : service_area is 1 : 0/N, but almost 1:N (~1k pwsid's missing, N=1.2 mean)
 - water_system : geographic_area is 1 : 0/1, but almost 1:1 (~1k pwsid's missing)
-
-Here are the useful columns we want from SDWIS and supplemental tables
-ws.pwsid - the PK
-ws.pws_name - name
-ws.pws_activity_code - active or not
-ws.pws_type_code - Filtered to "cws" only so maybe we don't need it
-ws.address_line1 - "The address applicable to the legal entity", whatever that means
-ws.address_line2
-ws.city_name
-ws.zip_code
-ws.primacy_agency_code
-wsf.facility_id - Optional. This denormalizes the data substantially.
-sa.service_area_type_code - for municipal vs mobile home park
-ga.city_served - this column is not populated in ws unfortunately
-ga.county_served - Maybe this will be helpful?
 """
 
 #########
@@ -88,10 +64,6 @@ sdwis_ga = pd.read_csv(
     usecols=["pwsid", "city_served", "county_served"],
     dtype="string")
 
-# Verify: pwsid is unique
-if not sdwis_ga["pwsid"].is_unique:
-    raise Exception("Failed assumption: pwsid in geographic_area is assumed to be unique")
-
 sdwis = sdwis.merge(sdwis_ga, on="pwsid", how="left")
 
 #########
@@ -120,90 +92,6 @@ if not sdwis["pwsid"].is_unique:
     raise Exception("Expected sdwis pwsid to be unique")
 
 sdwis.head()
-
-"""
-# Supplement with water_system_facilities?
-# This would roughly denormalize by 10x...probably don't want to do that yet.
-
-# water_system_facilities - PWSID + facility_id is unique
-# There are ~300 PWSID's in water_system but not water_system_facilities
-sdwis_wsf = pd.read_csv(DATA_PATH + "/sdwis_water_system_facility.csv")
-
-# Filter to only pws's that we're interested in
-sdwis_wsf = sdwis_wsf.loc[sdwis_wsf["pwsid"].isin(sdwis["pwsid"])]
-
-sdwis_wsf.head()
-"""
-
-#%% ##########################################
-# 2) FRS (EPA "Facility Registry Service")
-# FRS data model: https://www.epa.gov/frs/frs-physical-data-model
-##############################################
-
-"""
-Useful columns:
-pgm_sys_id - 
-    This is pretty much the primary key. (69,697 unique / 69,722 total). Dupes might be inactives
-    The key varies based on INTEREST_TYPE:
-    - When INTEREST_TYPE is "WATER TREATMENT PLANT" it's a pwsid + " " + facility_id
-    - For all others, it appears to be a pwsid
-active_status - 55k are "ACTIVE". Others have "INACTIVE", "CHANGED TO NON-PUBLIC SYSTEM", OR N/A
-state_code - The state in which the facility is located
-latitude83 - Lat of the facility
-longitude83 - Long of the facility
-accuracy_value - Measure of accuracy (in meters) of the lat/long
-collect_mth_desc - Text describing how lat/long was calculated
-ref_point_desc - Name that identifies the place for which geographic coordinates were established.
-"""
-
-frs = gpd.read_file(
-    DATA_PATH + "/frs.geojson")
-
-# keep_columns = [
-#     "PGM_SYS_ID", "LATITUDE83", "LONGITUDE83", "ACCURACY_VALUE", 
-#     "COLLECT_MTH_DESC", "REF_POINT_DESC", "geometry"]
-#
-#frs = frs[keep_columns]
-
-#%%
-
-# Assumption: PGM_SYS_ID is concatenated IFF INTEREST_TYPE == "WATER TREATMENT PLANT"
-if (~(frs["interest_type"] == "WATER TREATMENT PLANT") == (frs["pgm_sys_id"].str.contains(" "))).any():
-    raise Exception("Failed assumption: pgm_sys_id is concatenated IFF interest_type == 'WATER TREATMENT PLANT'")
-
-print(f"Unique pwsid's: {len(frs['pwsid'].unique())}")
-
-# 32,329 unique pwsid's after splitting apart the key.
-# We'll either need to join to water_system_facilities for water treatment plants,
-# Or we'll need to somehow aggregate the geometries
-frs.head()
-
-#%%
-# Almost every pwsid matches to the unfiltered sdwis list
-frs["pwsid"].isin(sdwis_unfiltered["pwsid"]).value_counts()
-
-#%%
-# But only 18k are CWS. Another 14k are non-community (TNCWS or NTNCWS)
-sdwis_unfiltered[sdwis_unfiltered["pwsid"].isin(frs["pwsid"])]["pws_type_code"].value_counts()
-
-#%%
-# Only 16k distinct matches are active CWS
-frs["matched"] = frs["pwsid"].isin(sdwis["pwsid"])
-len(frs[frs["matched"]]["pwsid"].unique())
-
-# 16k matches is far short of 50k matches.
-# But I went back to the raw FRS data and couldn't find any additional matches.
-
-# Since we successfully matched almost all FRS to unfiltered sdwis, it's not a matching problem.
-# Instead, it's an FRS problem. FRS simply appears to lack many pwsid's.
-
-# Options:
-#  - echo_exporter dataset instead. That had better lat/long
-
-#%%
-# Filter to only those that have a match on pwsid. 
-# 45k entries, but many are duplicated on facility_id
-frs = frs[frs["matched"]].drop(columns=["matched"])
 
 #%% ##########################################
 # 3) TIGER
@@ -248,24 +136,6 @@ echo: gpd.GeoDataFrame = gpd.GeoDataFrame(
     crs="EPSG:4326")
 
 echo.head()
-
-
-#%%
-# Are there any patterns when echo is in FRS vs when it's not in FRS?
-
-print("In FRS:")
-print(echo[echo["pwsid"].isin(frs["pwsid"])]["fac_collection_method"].value_counts())
-
-print("\nNot in FRS:")
-print(echo[~echo["pwsid"].isin(frs["pwsid"])]["fac_collection_method"].value_counts())
-
-# Yeah, interesting...
-# When it matches FRS:
-#  - fac_collection_method - Primarily "ADDRESS MATCHING-HOUSE NUMBER", "INTERPOLATION-PHOTO", "INTERPOLATION-MAP", etc.
-#  - fac_reference_point - Detailed info about the specific reference point where the centroid was picked at the facility
-# When it doesn't match FRS:
-#  - fac_collection_method - Primarily "County Centroid", "Zip Code Centroid", "State Centroid". Yuck.
-#  - fac_reference_point - Mostly null.
 
 #%% ##########################
 # SDWIS/ECHO <-> TIGER - Spatial + Name matching
@@ -360,7 +230,6 @@ print("Min: " + str(join_sub.groupby("geoid").size().min()))
 print("Max: " + str(join_sub.groupby("geoid").size().max()))
 
 # This seems like a decent stopping point.
-# TODO - Treat these as 3 match rules. Revisit the stacked match report, where multiple matches = higher strength
 
 #%% ##########################
 # Generate the final table
