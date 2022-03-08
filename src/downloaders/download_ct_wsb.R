2df7bac35f76f96736775fb0a84a843ec651d016
noorkb <noor@gosimplelab.com>
2022-03-08 13:43:02 -0800

    Update CT downloader url and unzip

--- src/downloaders/download_ct_wsb.R
+++ src/downloaders/download_ct_wsb.R
@@ -9,10 +9,16 @@ data_path <- Sys.getenv("WSB_DATA_PATH")
 options(timeout = 10000)
 
 # Data Source: Connecticut ArcGIS geojson water system boundary
-ct_url <- paste0("https://github.com/NIEPS-Water-Program/",
-                 "water-affordability/raw/main/data/ct_systems.geojson")
+ct_url <- paste0("https://portal.ct.gov/-/media/Departments-and-Agencies/",
+                 "DPH/dph/drinking_water/GIS/",
+                 "Buffered_Community_PWS_Service_Areas.zip")
 
-# create dir to store file and download
-dir_create(path(data_path, "boundary/ct"))
-download.file(ct_url, path(data_path, "boundary/ct/ct.geojson"))
+# create dir to store folder and download
+dir_path <- path(data_path, "boundary/ct")
+file_path <- path(dir_path, "ct.zip")
+dir_create(dir_path)
+download.file(ct_url, file_path)
 cat("Downloaded CT polygon boundary data.\n")
+
+# unzip folder
+unzip(zipfile=file_path, exdir=dir_path)
