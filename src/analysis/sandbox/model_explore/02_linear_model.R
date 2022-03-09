be83eab4fcb96189df590b64945d324aba67e371
RichPauloo <richpauloo@gmail.com>
2022-03-09 15:37:06 -0800

    linear modeling starts

--- src/analysis/sandbox/model_explore/02_linear_model.R
+++ src/analysis/sandbox/model_explore/02_linear_model.R
@@ -11,3 +11,13 @@
 #   - primacy_type: state, territory, tribal
 #   - primary_source_code: SW, GW, etc.
 
+library(tidyverse)
+library(tidymodels)
+library(sf)
+library(fs)
+
+staging_path <- Sys.getenv("WSB_STAGING_PATH")
+epsg_aw      <- Sys.getenv("WSB_EPSG_AW")
+epsg         <- as.numeric(Sys.getenv("WSB_EPSG"))
+
+d <- read_csv(path(staging_path, "matched_output_clean.csv"))
