94841e430c0fd3dba6e427ea7765908831728b82
RichPauloo <richpauloo@gmail.com>
2022-03-09 17:11:56 -0800

    add notes

--- src/analysis/sandbox/model_explore/02_linear_model.R
+++ src/analysis/sandbox/model_explore/02_linear_model.R
@@ -4,11 +4,13 @@
 #   - population_served_count: rescale, likely to have high leverage
 #   - service_connections_count
 #   - we have zip code: perhaps we use this to pull census vars
-#     or other vars like road density, population density
+#     or other vars like road density, population density, but we
+#     are unsure of data quality of zip
 
 # SDWIS water_system.csv categorical vars:
-#   - is_wholesaler_ind
-#   - primacy_type: state, territory, tribal
+#   - is_wholesaler_ind: complete and can use
+#   - primacy_type (state, territory, tribal): can't use because only 
+#     "state" observations have labeled radii (not territory or tribal)
 #   - primary_source_code: SW, GW, etc.
 
 library(tidyverse)
