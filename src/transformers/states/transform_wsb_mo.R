9657d732c663fcf5f6f0037950133209f19ca621
noorkb <noor@gosimplelab.com>
2022-03-14 12:15:45 -0700

    pws_id -> pwsid; changed pwsid to IPWS; split centroid

--- src/transformers/states/transform_wsb_mo.R
+++ src/transformers/states/transform_wsb_mo.R
@@ -34,6 +34,8 @@ mo_wsb <- mo_wsb %>%
     # importantly, area calculations occur in area weighted epsg
     st_areashape   = st_area(geometry),
     centroid       = st_geometry(st_centroid(geometry)),
+    centroid_x     = st_coordinates(centroid)[, 1],
+    centroid_y     = st_coordinates(centroid)[, 2],
     convex_hull    = st_geometry(st_convex_hull(geometry)),
     area_hull      = st_area(convex_hull),
     radius         = sqrt(area_hull/pi)
@@ -43,7 +45,7 @@ mo_wsb <- mo_wsb %>%
   # select columns and rename for staging
   select(
     # data source columns
-    pws_id           = PWSSID,
+    pwsid            = IPWS,
     pws_name         = PWSSNAME,
     state,
     county           = COUNTY,
@@ -52,7 +54,8 @@ mo_wsb <- mo_wsb %>%
     #    owner,
     # geospatial columns
     st_areashape,
-    centroid,
+    centroid_x,
+    centroid_y,
     area_hull,
     radius,
     geometry
