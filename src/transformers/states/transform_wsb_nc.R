9a781d5855e444ce5907a9c8677bc60360527e6e
noorkb <noor@gosimplelab.com>
2022-03-15 11:04:38 -0700

    change centroid epsg

--- src/transformers/states/transform_wsb_nc.R
+++ src/transformers/states/transform_wsb_nc.R
@@ -32,15 +32,18 @@ nc_wsb <- nc_wsb %>%
     WASYID         = paste0("NC", WASYID),
     # importantly, area calculations occur in area weighted epsg
     st_areashape   = st_area(geometry),
-    centroid       = st_geometry(st_centroid(geometry)),
-    centroid_x     = st_coordinates(centroid)[, 1],
-    centroid_y     = st_coordinates(centroid)[, 2],
     convex_hull    = st_geometry(st_convex_hull(geometry)),
     area_hull      = st_area(convex_hull),
     radius         = sqrt(area_hull/pi)
   ) %>%
   # transform back to standard epsg for geojson write
   st_transform(epsg) %>%
+  # compute centroids
+  mutate(
+    centroid       = st_geometry(st_centroid(geometry)),
+    centroid_long  = st_coordinates(centroid)[, 1],
+    centroid_lat   = st_coordinates(centroid)[, 2],
+  ) %>%
   # select columns and rename for staging
   select(
     # data source columns
@@ -53,8 +56,8 @@ nc_wsb <- nc_wsb %>%
 #    owner,
     # geospatial columns
     st_areashape,
-    centroid_x,
-    centroid_y,
+    centroid_long,
+    centroid_lat,
     area_hull,
     radius,
     geometry
