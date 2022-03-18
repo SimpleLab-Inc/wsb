5b24dbe035821ea609a208b4ffd1ee71d8a32621
RichPauloo <richpauloo@gmail.com>
2022-03-17 18:28:02 -0700

    impute service connection count instead of pop because it explains more variance

--- src/analysis/sandbox/model_explore/01_preprocess.R
+++ src/analysis/sandbox/model_explore/01_preprocess.R
@@ -15,28 +15,27 @@ j <- read_csv(path(staging_path, "matched_output.csv"),
   filter(!is.na(echo_latitude) | !is.na(echo_longitude)) %>% 
   # we also filter out 0 service connection count systems - 267 (0.4%), 
   # this should be cleaned/imputed in the transformer
-  filter(service_connections_count > 0) 
+  filter(population_served_count > 0) 
 cat("Read", nrow(j), 
     "matched outputs with nonzero service connection count.\n")
 
 
-# mean impute population_served == 0 with linear model --------------------
+# mean impute service connections == 0 with linear model ------------------
 
 # A 2022-03-08 meeting with IoW/BC/EPIC recommended filtering out 
 # wholesalers and water systems with a zero population count. However, 
-# many water systems have low population counts (e.g., between 0 and 10), 
-# but very high service connection count (e.g., in the hundreds to 
-# thousands), wholesalers in labeled data are primarily found in WA 
-# and TX, and wholesalers typically occupy urban areas and do not contain
-# smaller pwsids. Thus, we retain all observations and mean impute suspect
-# (between 0 and N) population counts.
+# many water systems have service connection counts (e.g., between 0 and 10), 
+# but very high population (e.g., in the hundreds to thousands), wholesalers 
+# in labeled data are primarily found in WA and TX, and wholesalers 
+# typically occupy urban areas and do not contain smaller pwsids. Thus, we 
+# retain all observations and mean impute suspect (between 0 and N) service
+# connections.
 
-# this is the critical population served count below which (inclusive) we
-# assume that the value is nonsensical, and impute it based on the service
-# connection count, which tends to be present and more correct.
+# this is the critical service connection count below which (inclusive) we
+# assume that the value is nonsensical, and impute it based on population
 n_max <- 10
-cat("Preparing to mean impute population served count", 
-    "for all population <=", n_max, ".\n")
+cat("Preparing to mean impute service connection count", 
+    "for all values <=", n_max, ".\n")
 
 # we learned in the Feb 2022 EDA (sandbox/eda/eda_february.Rmd) that
 # population served and service connection count had outliers that were 
@@ -53,21 +52,21 @@ cat("Preparing to mean impute population served count",
 
 # linear model for imputing population served from service connections.
 # Only train on population served not in 0:n_max (nonsensical)
-jm <- j %>% filter(! population_served_count %in% 0:n_max)
+jm <- j %>% filter(! service_connections_count %in% 0:n_max)
 
 # simple linear model for imputing service connection count and b1 slope
-m  <- lm(population_served_count ~ service_connections_count, data = jm)
-b1 <- coefficients(m)["service_connections_count"]
+m  <- lm(service_connections_count ~ population_served_count, data = jm)
+b1 <- coefficients(m)["population_served_count"]
 
-# predict, & change the y-intercept to 0 to avoid negative pop
+# predict, & change the y-intercept to 0 to avoid negative connections
 j <- j %>%
   mutate(
-    population_served_count = ifelse(
-      population_served_count %in% 0:10,
-      ceiling(service_connections_count * b1),
-      population_served_count)
+    service_connections_count = ifelse(
+      service_connections_count %in% 0:10,
+      ceiling(population_served_count * b1),
+      service_connections_count)
   )
-cat("Mean imputed population served count.\n")
+cat("Mean imputed service connection count.\n")
 
 
 # recalculate area, radius for multipolygon pwsids in labeled data --------
@@ -174,6 +173,33 @@ cat("Joined matched output, labeled data, and sdwis data.\n")
 # sanity row count equivalence pre and post join (this is FALSE when, for 
 # instance, duplicate pwsid are present)
 cat("Row count equivalence pre and post-join is", nrow(d) == nrow(j), "\n")
+
+
+# apply cleaning informed by EDA ------------------------------------------
+
+d <- d %>% 
+  mutate(
+    # when radius == 0, make it NA
+    radius = ifelse(radius == 0, NA, radius),
+    # split type codes in the "python list" into chr vectors
+    satc = strsplit(service_area_type_code, ", "),
+    # map over the list to remove brackets ([]) and quotes (')
+    satc = map(satc, ~str_remove_all(.x, "\\[|\\]|'")),
+    # sort the resulting chr vector
+    satc = map(satc, ~sort(.x)), 
+    # collapse the sorted chr vector
+    satc = map_chr(satc, ~paste(.x, collapse = "")),
+    # convert the sorted chr vector to factor with reasonable level count
+    satc = fct_lump_prop(satc, 0.02), 
+    satc = as.character(satc), 
+    satc = ifelse(is.na(satc), "Other", satc),
+    # convert T/F is_wholesaler_ind to character for dummy var prep
+    is_wholesaler_ind = ifelse(is_wholesaler_ind == TRUE, 
+                               "wholesaler", "not wholesaler"),
+    # make native american owner types (only 2 present) public/private (M)
+    owner_type_code = ifelse(owner_type_code == "N", "M", owner_type_code)
+  )
+cat("Cleaned data according to EDA-generated insights.\n")
 
 # write for modeling
 write_csv(d, path(staging_path, "matched_output_clean.csv"))
