c4a765b05e01106e5b7a6bcdd472da7dd013fdeb
Jess Goddard <jess@gosimplelab.com>
2022-03-21 07:43:23 -0700

    minor linting

--- src/downloaders/download_ar_wsb.R
+++ src/downloaders/download_ar_wsb.R
@@ -5,19 +5,22 @@ library(fs)
 # path to save raw data
 data_path <- Sys.getenv("WSB_DATA_PATH")
 
-# Allow for longer timeout to map download file
+# Allow for longer timeout to download file
 options(timeout = 10000)
 
 # Data Source: Arkansas ArcGIS shapefile water system boundary
 url <- paste0("https://geostor-vectors.s3.amazonaws.com/Utilities/SHP/",
               "PUBLIC_WATER_SYSTEMS.zip")
 
-# create dir to store folder and download
+# directory and file path
 dir_path <- path(data_path, "boundary/ar")
 file_path <- path(dir_path, "ar.zip")
+
+# create directory, download file and unzip
 dir_create(dir_path)
 download.file(url, file_path)
-cat("Downloaded AR polygon boundary data.\n")
-
-# unzip folder
 unzip(zipfile=file_path, exdir=dir_path)
+
+cat("Downloaded and unzipped AR polygon boundary data.\n")
+
+
