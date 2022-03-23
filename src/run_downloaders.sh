c0ddb93d1e863f162efc1599d9ef0d3f3e7976a6
noorkb <noor@gosimplelab.com>
2022-03-23 12:56:44 -0700

    remove colons

--- src/run_downloaders.sh
+++ src/run_downloaders.sh
@@ -6,37 +6,42 @@ SECONDS=0
 # run download scripts
 
 echo -e "\n\n--------------------------------------\n\n"
-echo -e "Downloading water system boundary data for states:"
+echo -e "Downloading water system boundary data for states"
 echo -e "\n\n--------------------------------------\n\n"
 find src/downloaders/states -type f -name "*.R" -exec Rscript {} \;
 
 echo -e "\n\n--------------------------------------\n\n"
-echo -e "Downloading ECHO admin data:"
+echo -e "Downloading ECHO admin data"
 echo -e "\n\n--------------------------------------\n\n"
 Rscript -e "source('src/downloaders/download_echo.R');"
 
 echo -e "\n\n--------------------------------------\n\n"
-echo -e "Downloading FRS centroids:"
+echo -e "Downloading FRS centroids"
 echo -e "\n\n--------------------------------------\n\n"
 Rscript -e "source('src/downloaders/download_frs.R');"
 
 echo -e "\n\n--------------------------------------\n\n"
-echo -e "Downloading mobile home parks point data:"
+echo -e "Downloading mobile home parks point data"
 echo -e "\n\n--------------------------------------\n\n"
 Rscript -e "source('src/downloaders/download_mhp.R');"
 
 echo -e "\n\n--------------------------------------\n\n"
-echo -e "Downloading SDWIS data:"
+echo -e "Downloading SDWIS data"
 echo -e "\n\n--------------------------------------\n\n"
 python src/downloaders/download_sdwis.py
 
 echo -e "\n\n--------------------------------------\n\n"
-echo -e "Downloading TIGRIS places and Natural Earth coastline:"
+echo -e "Downloading TIGRIS places and Natural Earth coastline"
 echo -e "\n\n--------------------------------------\n\n"
 Rscript -e "source('src/downloaders/download_tigris_ne.R');"
 
 echo -e "\n\n--------------------------------------\n\n"
-echo -e "Downloading UCMR occurrence data:"
+echo -e "Downloading SDWIS data ( delete this !!!! )"
+echo -e "\n\n--------------------------------------\n\n"
+python src/downloaders/download_sdwis.py
+
+echo -e "\n\n--------------------------------------\n\n"
+echo -e "Downloading UCMR occurrence data"
 echo -e "\n\n--------------------------------------\n\n"
 Rscript -e "source('src/downloaders/download_ucmr.R');"
 
