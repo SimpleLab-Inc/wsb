f473c9729976997ab11dddf106e87f7e3497b06b
Ryan Shepherd <ryan@databrook.io>
2022-03-25 12:30:34 -0700

    Preserve zip code in UCMR mapping

--- src/analysis/sandbox/matching/map_ucmr.py
+++ src/analysis/sandbox/matching/map_ucmr.py
@@ -17,26 +17,34 @@ EPSG = os.environ["WSB_EPSG"]
 
 ucmr = gpd.read_file(DATA_PATH + "/ucmr.geojson")
 
+#%%
+
 # TODO: Could we get counties in this? Not currently present, but we could look it up from zip code.
 # TODO: Could we get lat/long of centroid?
 
+# Preserve the original data in memory (for troubleshooting)
+agg = ucmr
+
 # Remove empty geometries
-ucmr = ucmr[(~ucmr["geometry"].is_empty) & ucmr["geometry"].notna()]
+#agg = agg[(~agg["geometry"].is_empty) & agg["geometry"].notna()]
 
 # Aggregate polygons so pwsid is unique
-ucmr = (ucmr[["pwsid", "geometry"]]
-    .dissolve(by="pwsid")
+# Dissolve function:
+#  (1) Aggregates polygons with a unary_union and
+#  (2) Aggregates other fields with a "first". This means we lose zip codes...but our model only allows for one anyway.
+agg = (agg[["pwsid", "zipcode", "geometry"]]
+    .dissolve(by="pwsid", aggfunc="first")
     .reset_index())
-
 
 #%%
 
 df = gpd.GeoDataFrame().assign(
-    source_system_id    = ucmr["pwsid"],
+    source_system_id    = agg["pwsid"],
     source_system       = "ucmr",
-    xref_id             = "ucmr." + ucmr["pwsid"],
-    master_key          = ucmr["pwsid"],
-    geometry            = ucmr["geometry"],
+    xref_id             = "ucmr." + agg["pwsid"],
+    master_key          = agg["pwsid"],
+    zip                 = agg["zipcode"],
+    geometry            = agg["geometry"],
     geometry_quality    = "Zip code boundary"
 )
 
