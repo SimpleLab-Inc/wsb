8e03313951128563d5ca71107aa0c75d451804fc
RichPauloo <richpauloo@gmail.com>
2022-03-26 17:14:24 -0700

    print messaging and improved docs

--- src/analysis/sandbox/model_explore/combine_tiers.R
+++ src/analysis/sandbox/model_explore/combine_tiers.R
@@ -1,32 +1,37 @@
 # combine tamm tiers into one spatial layer and write ---------------------
 
+cat("\n\nPreparing to combine/write spatial output for TAMM Tiers 1-3.\n")
+
 library(tidyverse)
 library(sf)
 library(fs)
 library(here)
 library(glue)
 
-# load environmental variables 
 staging_path <- Sys.getenv("WSB_STAGING_PATH")
 epsg         <- as.numeric(Sys.getenv("WSB_EPSG"))
 
 
-# load tiered spatial data ------------------------------------------------
+# load geometries for each tier -------------------------------------------
+
+cat("Loading geometries for Tiers 1-3...") 
 
 # Tier 1: ASSIMILATED labeled boundaries
 t1 <- path(staging_path, "wsb_labeled_clean.geojson") %>% 
-  st_read() %>% 
+  st_read(quiet = TRUE) %>% 
   select(pwsid)
 
 # Tier 2: MATCHED TIGER Place boundaries
 t2 <- path(staging_path, "tigris_places_clean.geojson") %>% 
-  st_read() %>% 
+  st_read(quiet = TRUE) %>% 
   select(tiger_match_geoid = geoid)
 
 # Tier 3: MODELED boundaries
 t3 <- path(staging_path, "tier3_median.geojson") %>% 
-  st_read() %>% 
+  st_read(quiet = TRUE) %>% 
   select(pwsid)
+
+cat("done.\n") 
 
 
 # matched output and tier classification ----------------------------------
@@ -43,10 +48,12 @@ cols_select <- c(
 # service connection count cutoff for community water systems
 n_max <- 15
 
-# matched output
+# read and format matched output
+cat("Reading matched output...")
+
 d <- read_csv(path(staging_path, "matched_output_clean.csv")) %>%
   mutate(
-    # indicate the tier of the wsb
+    # indicate the tier to use for each pwsid
     tier = case_when(
       has_labeled_bound == TRUE ~ "Tier 1",
       has_labeled_bound == FALSE & !is.na(tiger_match_geoid) ~ "Tier 2",
@@ -60,23 +67,27 @@ d <- read_csv(path(staging_path, "matched_output_clean.csv")) %>%
   # remove 834 rows (1.5% of data) not in contiguous US, mostly Puerto Rico
   filter(primacy_agency_code %in% state.abb) %>% 
   # select only relevant cols
-  select(all_of(cols_select))
+  select(all_of(cols_select)) %>% 
+  suppressMessages()
+
+cat("done.\n") 
 
 
 # combine tiers -----------------------------------------------------------
 
-# Separate Tiers 1-3 from matched output and join to spatial data
+# Separate Tiers 1-3 from matched output, join to spatial data, and bind
 dt1 <- d %>% filter(tier == "Tier 1") %>% left_join(t1) %>% st_as_sf()
 dt2 <- d %>% filter(tier == "Tier 2") %>% left_join(t2) %>% st_as_sf() 
 dt3 <- d %>% filter(tier == "Tier 3") %>% left_join(t3) %>% st_as_sf()
 
-# TAMM Tiered layer
 tamm <- bind_rows(dt1, dt2, dt3)
+
+cat("Combined spatial data using best available tiered data.\n")
 
 
 # write to multiple output formats ----------------------------------------
 
-# paths to write to
+# paths to write
 path_geojson  <- here("tamm_layer",     glue("{Sys.Date()}_tamm.geojson"))
 path_shp      <- here("tamm_layer/shp", glue("{Sys.Date()}_tamm.shp"))
 path_csv      <- here("tamm_layer",     glue("{Sys.Date()}_tamm.csv"))
@@ -85,3 +96,5 @@ path_csv      <- here("tamm_layer",     glue("{Sys.Date()}_tamm.csv"))
 st_write(tamm, path_geojson, delete_dsn   = TRUE)
 st_write(tamm, path_shp,     delete_layer = TRUE)
 tamm %>% st_drop_geometry() %>% write_csv(path_csv)
+
+cat("Wrote data to geojson, shp, csv.\n\n\n")
