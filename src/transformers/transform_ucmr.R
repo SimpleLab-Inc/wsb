03a1a5dd7a56042a99d3d2346001fb5d8864c291
noorkb <noor@gosimplelab.com>
2022-03-29 17:03:56 -0700

    Added pwsid deduplication and geometry combination code

--- src/transformers/transform_ucmr.R
+++ src/transformers/transform_ucmr.R
@@ -79,6 +79,81 @@ ucmr <- ucmr %>%
   st_as_sf(crs = epsg)
 
 
+# combine data and merge geometries for rows with duplicate pwsids --------
+
+# show there are rows with duplicate pwsids
+multi <- st_drop_geometry(ucmr) %>% 
+  count(pwsid, sort = TRUE) %>% 
+  filter(n > 1)
+cat("Detected", nrow(multi), "groups of rows with duplicate pwsids.\n")
+
+# add column indicating if row has a duplicated pwsid
+ucmr <- ucmr %>% 
+  # label duplicated pwsid geometries
+  mutate(is_multi = ifelse(pwsid %in% multi$pwsid, TRUE, FALSE))
+
+# separate rows without duplicated pwsids
+ucmr_no_multi <- ucmr %>% 
+  filter(is_multi == FALSE) %>% 
+  # remove is_multi column
+  select(-is_multi)
+
+
+# for rows with duplicated pwsids: 
+# union geometries, recalculate area, centroids, radius
+ucmr_multi <- ucmr %>% 
+  # filter for rows with duplicated pwsid's
+  filter(is_multi == TRUE) %>% 
+  st_make_valid() %>% 
+  # importantly, all calculations take place in AW epsg 
+  st_transform(epsg_aw) %>% 
+  st_make_valid() %>% 
+  group_by(pwsid) %>% 
+  # mutate these new columns, knowing full well that duplicate rows
+  # will be created, but that they will be dropped in the next step
+  mutate(
+    # combine all fragmented geometries
+    geometry       = st_union(geometry),
+    # new area is the sum of the area of all polygons
+    st_areashape   = sum(st_areashape),
+    area_hull      = sum(area_hull),
+    # new radius is calculated from the new area
+    radius         = sqrt(area_hull/pi),
+    # combine data into list-formatted strings for character columns
+    across(where(is.character), ~toString(unique(.)))
+  ) %>%
+  # only take the first result from each group
+  slice(1) %>% 
+  ungroup() %>% 
+  # convert back to the project standard epsg
+  st_transform(epsg) %>% 
+  st_make_valid() %>% 
+  # compute new centroids and note that when multipolygons are separated
+  # by space, these are suspect and should not be used. Importantly, this
+  # calculation occurs in the EPSG consistent with other staged data!
+  mutate(
+    centroid       = st_geometry(st_centroid(geometry)),
+    centroid_long  = st_coordinates(centroid)[, 1],
+    centroid_lat   = st_coordinates(centroid)[, 2]
+  ) %>% 
+  # remove centroid, area_hull, and is_multi columns
+  select(-c(centroid, area_hull, is_multi))
+
+cat("Recalculated area, radius, centroids for multipolygon pwsids.\n")
+cat("Combined string values for multipolygon pwsids.\n")
+
+# combine ucmr labeled data with corrected rows
+ucmr_clean <- bind_rows(ucmr_no_multi, ucmr_multi)
+
+# verify that there is only one pwsid per geometry
+n <- ucmr_clean %>%
+  st_drop_geometry() %>%
+  count(pwsid) %>%
+  filter(n > 1) %>%
+  nrow()
+cat(n, "duplicate pwsids in labeled data following fix.\n")
+
+
 # Write clean ucmr data to geojson
 path_out <- path(staging_path, "ucmr.geojson")
 if(file_exists(path_out)) file_delete(path_out)
